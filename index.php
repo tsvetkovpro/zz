e691f531a673f26bb29872661c7429d0895869b1
tsvetkovpro <285019@gmail.com>
2017-08-07 13:50:31 +0500

    fav add img with people

--- index.php
+++ index.php
@@ -9,30 +9,18 @@ include_once "functions.php"
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <link rel="apple-touch-icon" sizes="57x57" href="/images/favicon/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="/images/favicon/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/images/favicon/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="/images/favicon/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/images/favicon/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/images/favicon/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/images/favicon/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/images/favicon/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="/images/favicon/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
-  <link rel="manifest" href="/images/favicon/manifest.json">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="/images/favicon/ms-icon-144x144.png">
-  <meta name="theme-color" content="#ffffff">
+  <meta property="og:image" content="images/logo.png">
+  <link rel="shortcut icon" href="images/favic/favicon.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" href="images/favic//apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="images/favic/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="images/favic/apple-touch-icon-114x114.png">
 
   <!-- Chrome, Firefox OS and Opera -->
-  <meta name="theme-color" content="#027a3a">
+  <meta name="theme-color" content="#027A3A">
   <!-- Windows Phone -->
-  <meta name="msapplication-navbutton-color" content="#027a3a">
+  <meta name="msapplication-navbutton-color" content="#027A3A">
   <!-- iOS Safari -->
-  <meta name="apple-mobile-web-app-status-bar-style" content="#027a3a">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#027A3A">
 
   <title>Сберегательные программы кредитного потребительского кооператива СОЮЗ-ЗБЕРЗАЙМ</title>
   <style>
