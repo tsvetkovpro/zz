a4298409d26af8e69fb3346e406c663d2c715236
tsvetkovpro <285019@gmail.com>
2017-08-08 09:18:51 +0500

    fix send empty email

--- send_email.php
+++ send_email.php
@@ -5,36 +5,46 @@ include "functions.php";
 // контактная форма
 if (isset($_POST['send_form'])) {
 
-  $name = $_POST['user_name'];
-  $phone = $_POST['user_phone'];
+  $name = $_POST['name'];
+  $phone = $_POST['phone'];
 
-  $title = 'Заявка с сайта ZберZайм';
-  $from = 'kpkzberzaim@gmail.com';
-  $to = 'kpkzberzaim@gmail.com';
-  $text = '
-  <u>
-    <b>
-      Внимание!
-    </b>
-      Поступила заявка с сайта ЗберЗайм
-  </u>
-  <br><br>
-    //---------- начало сообщения ----------//
-  <br><br>
-    <b>Имя клиента: '.$name.'
-    <br>
-    Номер телефона для связи: <b>'.$phone.'</b>
-  <br><br>
-    //---------- конец сообщения ----------//
-  ';
 
-  $send_email = send_email($title, $from, $to, $text);
+  if (!empty($name) && !empty($phone)) {
+    
+    $title = 'Заявка с сайта ZберZайм';
+    $from = 'kpkzberzaim@gmail.com';
+    $to = 'kpkzberzaim@gmail.com';
+    $text = '
+    <u>
+      <b>
+        Внимание!
+      </b>
+        Поступила заявка с сайта ЗберЗайм
+    </u>
+    <br><br>
+      //---------- начало сообщения ----------//
+    <br><br>
+      <b>Имя клиента: '.$name.'
+      <br>
+      Номер телефона для связи: <b>'.$phone.'</b>
+    <br><br>
+      //---------- конец сообщения ----------//
+    ';
 
-  if ($send_email) {
-    echo 'success';
+    $send_email = send_email($title, $from, $to, $text);
+
+    if ($send_email) {
+      echo 'success';
+    } else {
+      echo 'fail';
+    }
+
   } else {
     echo 'fail';
   }
+
+
+  
 
 }
 
